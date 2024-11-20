057d3867b751056b5e7d2dece87fc4487f68a613
austinbhan <austinbhan@outlook.com>
2024-11-20 10:02:48 -0800

    Imported navbar assets

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
-// import Home from "./pages/Home";
-// import News from "./pages/News";
+import Home from "./Pages/Home";
+import News from "./Pages/News";
 
 function App() {
   return (
@@ -11,8 +11,8 @@ function App() {
       <Navbar />
       <main className="main-content">
         <Routes>
-          {/* <Route path="/" element={<Home />} />
-          <Route path="/news" element={<News />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<News />} />
         </Routes>
       </main>
     </Router>
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,63 +1,90 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { IoClose, IoMenu } from 'react-icons/io5';
-import "./Navbar.css"
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { IoClose, IoMenu } from "react-icons/io5";
+import "./Navbar.css";
 
-export default function Navbar() {
-    return (
-        <header className="header">
-            <nav className="nav container">
-                <NavLink to="/" className="nav__logo">
-                Navigation Bar
-                </NavLink>
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
 
-                <div className={"nav__menu"}
-                id="nav-menu">
-                    <ul className="nav__list">
-                        <li className="nav__item">
-                            <NavLink to="/" className="nav__link">
-                            Home
-                            </NavLink>
-                        </li>
-                        <li className="nav__item">
-                            <NavLink to="/news" className="nav__link">
-                            News
-                            </NavLink>
-                        </li>
-                        <li className="nav__item">
-                            <NavLink
-                            to="/about-us"
-                            className="nav__link">
-                                About Us
-                            </NavLink>
-                        </li>
-                        <li className="nav__item">
-                            <NavLink 
-                            to="/favorite"
-                            className="nav__link">
-                                Favorite
-                            </NavLink>
-                        </li>
-                        <li className="nav__item">
-                            <NavLink
-                            to="/location"
-                            className="nav__link">
-                                Location
-                            </NavLink>
-                        </li>
-                    </ul>
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
-                    <div className="nav__close" id="nav-close">
-                        <IoClose />
-                    </div>
+  const closeMenuOnMobile = () => {
+    if (window.innerWidth <= 1150) {
+      setShowMenu(false);
+    }
+  };
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <NavLink to="/" className="nav__logo">
+          Navigation Bar
+        </NavLink>
 
-                    <div className="nav__toggle" id="nav-toggle">
-                        <IoMenu />
-                    </div>
-                </div>
+        <div
+          className={`nav__menu ${showMenu ? "show-menu" : ""}`}
+          id="nav-menu"
+        >
+          <ul className="nav__list">
+            <li className="nav__item">
+              <NavLink to="/" className="nav__link" onClick={closeMenuOnMobile}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/news"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                News
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/about-us"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                About Us
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/favorite"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                Favorite
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/location"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                Location
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/get-started" className="nav__link nav__cta">
+                Get Started
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav__close" id="nav-close" onClick={toggleMenu}>
+            <IoClose />
+          </div>
+        </div>
 
-            </nav>
-        </header>
-    );
+        <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+          <IoMenu />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
+export default Navbar;
